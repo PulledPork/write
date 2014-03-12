bbe3be764ac8d3ca35c41f703a17220893d0fc8f
Josh <josh7weaver@gmail.com>
2014-03-12 15:31:10 -0400

    added comments to controller, model, and new.html file. Setup to build out the secure password and validation system. Need to add password and password_confirmation to the DB. I think that works with the has_secure_password command

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
 end
+
+# For more info see:
+# partial - http://ruby.railstutorial.org/chapters/modeling-users#code-validates_format_of_email
+# 
+# FUll HTML: 
+# http://ruby.railstutorial.org/chapters/modeling-users#sec-has_secure_password
+
+### >> search for "USER.RB" in ch 6
