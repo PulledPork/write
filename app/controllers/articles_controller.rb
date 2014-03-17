25131a7e17a6291b1b200e6716d70478c5aac876
Joshua Weaver <josh7weaver@gmail.com>
2014-03-17 11:29:22 -0400

    fixed editing funct.
    You can now Edit an existing article!
    
    added update method to Article conroller (article#update)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,35 @@ class ArticlesController < ApplicationController
 	def edit
 		@article = Article.find(params[:id])
 	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	private
+		def article_params
+			params.require(:Article).permit(:title, :text)
+		end
+
 end
+
+
+=begin
+
+def update
+  @post = Post.find(params[:id])
+ 
+  if @post.update(params[:post].permit(:title, :text))
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+=end
