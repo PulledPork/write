1abd9ec8a5976ffb4559c62cea5a963def1253d8
Joshua Weaver <josh7weaver@gmail.com>
2014-03-18 16:43:25 -0400

    Adding User<Article 1toMany Relationship
    > updated article & user model
    > added DB migration for user_id column (can use user_id:relationship in
    the rails generate command to do the same thing
    > added code in view to show the article authors name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+	has_many :articles, dependent: :destroy
+	# see http://ruby.railstutorial.org/chapters/user-microposts#code-micropost_dependency
+
+
 end
 
 # For more info see:
-# partial - http://ruby.railstutorial.org/chapters/modeling-users#code-validates_format_of_email
+# http://ruby.railstutorial.org/chapters/modeling-users#code-validates_format_of_email
 # 
 # FUll HTML: 
 # http://ruby.railstutorial.org/chapters/modeling-users#sec-has_secure_password
