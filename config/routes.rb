3b1e5165b2528b19f51787d4c0b49a8218977f3d
Joshua Weaver <josh7weaver@gmail.com>
2014-03-24 15:33:53 -0400

    Got the publish action working
    Finally got routes working... just needed to add "GET" action instead of
    Post or patch. may update this solution

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Write::Application.routes.draw do
 
   resources :articles, only: :index
   
+  #match 'articles/:id/publish', to: 'articles#publish', via: [:get, :post] 
+  #post 'articles/:id/publish' => 'articles#publish'
+  
+
   resources :users do
     resources :articles, shallow: true, except: :index do
       member do
-        post 'publish'
+        get 'publish'  
+        # This only works for get? at least the url that's generated
+        # passes params via GET -- url is "publish_article_path(@article.id)"
       end
     end
   end
@@ -18,7 +24,6 @@ Write::Application.routes.draw do
   ## end
   ## resources :articles, only: [:show, :edit, :update, :destroy]
 
-  #patch 'articles/:id' => 'articles#publish'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
