d54fdfc1ce95526a8b3b707523cc4f81b9c4aa85
Joshua Weaver <josh7weaver@gmail.com>
2014-03-28 17:01:47 -0400

    Session Code, still IP
    a lot happened lol:
    
    http://ruby.railstutorial.org/book/ruby-on-rails-tutorial#sec-current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,17 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		
+		user = User.find_by(email: params[:session][:email].downcase)
+
+		if user && user.authenticate(params[:session][:password])
+			#they are who they say they are, show them profile
+			sign_in(user)	# custom function
+			redirect_to user
+		else
+			#they lie! show errors
+			flash.now[:danger] = "Invalid email/password combination"
+			render 'new'
+		end
 	end
 
 	def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }		# make email lowercase
+	before_create :create_remember_token
+
 	# see http://ruby.railstutorial.org/chapters/user-microposts#code-micropost_dependency
 	has_many :articles, dependent: :destroy
 
@@ -7,7 +10,6 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }  # validate password length
 
 	# -- Validate EMAIL
-	before_save { self.email = email.downcase }					# make email lowercase
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence:   true,							# validate email presence
 	                format:     { with: VALID_EMAIL_REGEX },	# validate email format
@@ -16,6 +18,19 @@ class User < ActiveRecord::Base
 	# -- Validate NAME
 	validates :name, presence: true, length: { maximum: 50 }	# validate presence/length of name
 	
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.hash(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+		def create_remember_token
+		  self.remember_token = User.hash(User.new_remember_token)
+		end
 end
 
 # For more info see:
